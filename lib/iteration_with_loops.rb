ec2aea11db603d61d6beca6ce0b722160954541a
Conrad Buys <conradbuys11@gmail.com>
2020-08-08 19:24:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,13 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  final_string = ""
+  
+  src.each do |outer_index|
+    src[outer_index].each do |inner_index|
+      if inner_index.is_s?
+        
+    end
+  end
+  
 end
